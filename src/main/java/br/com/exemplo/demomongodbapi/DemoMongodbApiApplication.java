06e7a0341b084b88aa122c40f0f18e92f726721b
Luis Lucana <luislucana@gmail.com>
2020-01-24 13:04:53 -0300

    Criação dos arquivos.

---
+++ src/main/java/br/com/exemplo/demomongodbapi/DemoMongodbApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.demomongodbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMongodbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMongodbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/exemplo/demomongodbapi/DemoMongodbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.demomongodbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMongodbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
