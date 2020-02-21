09cd592a96e80220891afe4795213cf813641704
Luis Lucana <luislucana@gmail.com>
2020-02-21 11:16:27 -0300

    Correções e ajustes.

--- src/main/java/br/com/demo/ApplicationStarter.java
+++ src/main/java/br/com/demo/ApplicationStarter.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * https://docs.spring.io/spring-boot/docs/2.0.8.RELEASE/reference/htmlsingle/#boot-features-connecting-to-mongodb
  * https://docs.spring.io/spring-data/mongodb/docs/2.2.4.RELEASE/reference/html/#reference
  * https://www.mongodb.com/blog/post/mongodb-multi-document-acid-transactions-general-availability
+ * https://medium.com/oril/uploading-files-to-aws-s3-bucket-using-spring-boot-483fcb6f8646
+ * https://github.com/joshlong/bookmarks/tree/tutorial
  */
 @SpringBootApplication
 public class ApplicationStarter {
